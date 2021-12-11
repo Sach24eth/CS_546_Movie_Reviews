b8d8f250cfaf405b2ea5bb0b3a884f6ee145f2d2
Leo B Jacob <leobjacob@gmail.com>
2021-12-11 12:57:29 -0500

    add and delete products rebase

--- server/data/products.js
+++ server/data/products.js
@@ -4,31 +4,31 @@ let { ObjectId } = require("mongodb");
 const { isValidObject, addhttp, isValidEmail } = require("../utils.js");
 
 function checkInputs(
-    productName,
-    description,
-    websiteUrl,
-    logo,
-    tags,
-    developer
+  productName,
+  description,
+  websiteUrl,
+  logo,
+  tags,
+  developer
 ) {
-    //If arguments are not provided
-    if (
-        !productName ||
-        !description ||
-        !websiteUrl ||
-        !tags ||
-        !developer ||
-        !logo
-    )
-        throw "Error: All arguments have not been provided";
-    // check if type is alright
-    if (typeof productName !== "string" || productName.trim().length < 1) {
-        throw "Error: productName is not strings";
-    }
-    if (typeof description !== "string" || description.trim().length < 1)
-        throw "Error Descritpion is not a string";
-    if (typeof websiteUrl !== "string" || websiteUrl.trim().length < 1)
-        throw "Error: website_Url is not a string";
+  //If arguments are not provided
+  if (
+    !productName ||
+    !description ||
+    !websiteUrl ||
+    !tags ||
+    !developer ||
+    !logo
+  )
+    throw "Error: All arguments have not been provided";
+  // check if type is alright
+  if (typeof productName !== "string" || productName.trim().length < 1) {
+    throw "Error: productName is not strings";
+  }
+  if (typeof description !== "string" || description.trim().length < 1)
+    throw "Error Descritpion is not a string";
+  if (typeof websiteUrl !== "string" || websiteUrl.trim().length < 1)
+    throw "Error: website_Url is not a string";
 
   let re =
     /^(http:\/\/|https:\/\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[‌​a-z]{2}\.([a-z]+)?$/gm;
@@ -44,6 +44,7 @@ function checkInputs(
     if (typeof tags[i] !== "string" || tags[i].trim().length < 1) {
       throw "Error: Tag is not of string type or tag field is empty";
     }
+  }
 }
 
 //
@@ -142,60 +143,59 @@ let exportedMethods = {
     return sortedNameBylikes;
   },
 
-    //
-    // This function will get a product by tag
-    //
-    async getProductbyTag(tagSearch) {
-        if (typeof tagSearch !== "string")
-            throw "Error: The input is not a string";
-        tagSearch = tagSearch.toLowerCase();
-        const query = new RegExp(tagSearch, "i");
-        const productCollection = await products();
-        const productByTag = await productCollection
-            .find({
-                tags: { $regex: query },
-            })
-            .toArray();
-        if (productByTag.length === 0) throw "Error: No Matches";
-        const soredTagByLikes = productByTag.sort(productByTag.likes);
-        return soredTagByLikes;
-    },
-    async updateCount(prodId, liked) {
-        let objId = ObjectId(prodId);
-        const productCollection = await products();
-        const product = await productCollection.findOne({ _id: objId });
-        let updated_like;
-        if (product === null) throw "No Product with this ID";
-        if (liked) {
-            updated_like = parseInt(product.likes) + 1;
-        } else {
-            updated_like = parseInt(product.likes) - 1;
-        }
-        const updated_detials = { likes: updated_like };
-        const updatedInfo = await productCollection.updateOne(
-            { _id: objId },
-            { $set: updated_detials }
-        );
-        if (updatedInfo.modifiedCount === 0) {
-            throw "Could not update the product because it was not found in the database";
-        }
-    },
+  //
+  // This function will get a product by tag
+  //
+  async getProductbyTag(tagSearch) {
+    if (typeof tagSearch !== "string") throw "Error: The input is not a string";
+    tagSearch = tagSearch.toLowerCase();
+    const query = new RegExp(tagSearch, "i");
+    const productCollection = await products();
+    const productByTag = await productCollection
+      .find({
+        tags: { $regex: query },
+      })
+      .toArray();
+    if (productByTag.length === 0) throw "Error: No Matches";
+    const soredTagByLikes = productByTag.sort(productByTag.likes);
+    return soredTagByLikes;
+  },
+  async updateCount(prodId, liked) {
+    let objId = ObjectId(prodId);
+    const productCollection = await products();
+    const product = await productCollection.findOne({ _id: objId });
+    let updated_like;
+    if (product === null) throw "No Product with this ID";
+    if (liked) {
+      updated_like = parseInt(product.likes) + 1;
+    } else {
+      updated_like = parseInt(product.likes) - 1;
+    }
+    const updated_detials = { likes: updated_like };
+    const updatedInfo = await productCollection.updateOne(
+      { _id: objId },
+      { $set: updated_detials }
+    );
+    if (updatedInfo.modifiedCount === 0) {
+      throw "Could not update the product because it was not found in the database";
+    }
+  },
 
-    //function to delete product from the database
-    async deleteProduct(prodId) {
-        isValidObjectId(prodId);
-        prodId = ObjectId(prodId);
-        const prodList = await products();
-        const prodCheck = prodList.findOne({ _id: prodId });
-        if (!prodCheck) {
-            throw "Error: Product to be deleted was not found in the database";
-        }
-        const delProd = prodList.deleteOne({ _id: prodId });
-        if (delProd.deletedCount == 0) {
-            throw `Product ${delProd.name} could not be deleted`;
-        }
-        return `Product ${delProd.name} has been deleted successfully`;
-    },
+  //function to delete product from the database
+  async deleteProduct(prodId) {
+    isValidObjectId(prodId);
+    prodId = ObjectId(prodId);
+    const prodList = await products();
+    const prodCheck = prodList.findOne({ _id: prodId });
+    if (!prodCheck) {
+      throw "Error: Product to be deleted was not found in the database";
+    }
+    const delProd = prodList.deleteOne({ _id: prodId });
+    if (delProd.deletedCount == 0) {
+      throw `Product ${delProd.name} could not be deleted`;
+    }
+    return `Product ${delProd.name} has been deleted successfully`;
+  },
 
   async updateProduct(
     updId,
